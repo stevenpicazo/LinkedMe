01b6c4f37eb83614414e1fe8397f449043ffb070
stevenpicazo <stevenpicazo01@gmail.com>
2023-02-22 13:20:04 -0800

    Post Feature routes complete

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -1,17 +1,92 @@
 from flask import Blueprint, request
-from app.models import Post
+from app.models import db, Post
 from flask_login import login_required, login_user, current_user
-from app.forms import post_form
+from app.forms import PostForm
 from datetime import datetime
 
 post_routes = Blueprint('posts', __name__)
 
+##! GET ALL POSTS
 @post_routes.route('/')
 @login_required
 def get_posts():
     '''
-    Query for all posts.
+    Query's for all posts for logged in user.
     '''
+    if current_user:
+        all_posts = Post.query.all()
+        posts = [post.to_dict() for post in all_posts]
+        
+        return posts, 200
+
+##! CREATE A POST
+@post_routes.route('/', methods = ['POST'])
+@login_required
+def create_post():
+    '''
+    Creates post for logged in user
+    '''
+    form = PostForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        new_post = Post(
+            post = form.data['post'],
+            image = form.data['image'],
+            user_id = current_user.id,
+            created_at=datetime.now(),
+            updated_at=datetime.now()
+        )
+        
+        db.session.add(new_post)
+        db.session.commit()
+        return new_post.to_dict(), 201
     
-    posts = Post.query.all()
-    print(posts)
+    return {'errors': form.errors}, 401
+            
+##! UPDATE A POST
+@post_routes.route('/<int:post_id>', methods=['PUT'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get(post_id)
+
+    if not post:
+        return {'error': 'Post not found.'}, 404
+
+    if post.user_id != current_user.id:
+        return {'error': 'Unauthorized.'}, 401
+
+    form = PostForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        post.post = form.data['post']
+        post.image = form.data['image']
+        post.updated_at = datetime.now()
+        db.session.commit()
+
+        return post.to_dict(), 200
+
+    return {'errors': form.errors}, 401
+    
+    
+##! DELETE A SPOT
+@post_routes.route('/<int:post_id>', methods=['DELETE'])
+@login_required
+def delete_post(post_id):
+    '''
+    Deletes post for logged in user by post id
+    '''
+    post = Post.query.get(post_id)
+    
+    if not post:
+        return {'error': 'Post not found.'}, 404
+
+    if post.user_id != current_user.id:
+        return {'error': 'Unauthorized.'}, 401
+    
+    if post.user_id == current_user.id:
+        db.session.delete(post)
+        db.session.commit()
+        return {'message': 'Post deleted'}, 200
+
+     
+     
--- app/forms/post_form.py
+++ app/forms/post_form.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired, Length, URL
 
 class PostForm(FlaskForm):
     post = TextAreaField(
-        'post', validators=[DataRequired(), 
+        'post', validators=[DataRequired('This field is required'), 
         Length(min=1, max=3000, message='Posts are limited to 3000 characters')])
 
     image = StringField('image', validators=[URL(message='Please provide a valid image url')])
