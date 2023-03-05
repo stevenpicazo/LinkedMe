2b6694ce2487e973244003c9742c893fa6b36564
stevenpicazo <stevenpicazo01@gmail.com>
2023-03-05 11:12:11 -0800

    Sign up routes updated

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, jsonify
-from flask_login import login_required
-from app.models import User
+from flask import Blueprint, jsonify, request
+from flask_login import login_required, login_user, current_user
+from app.models import User, db
+from app.forms import SignUpForm
 
 user_routes = Blueprint('users', __name__)
 
@@ -23,3 +24,25 @@ def user(id):
     """
     user = User.query.get(id)
     return user.to_dict()
+
+@user_routes.route('/<int:id>/edit', methods=['PUT'])
+@login_required
+def edit_profile(id):
+    """
+    Query for a user by id and edits user info.
+    """
+    user = User.query.get(id)
+    if not user:
+        return {'errors': 'Page not found'}
+    
+    if user.id != current_user.id:
+        return {'errors': 'Unauthorized.'}, 401
+    
+    form = SignUpForm(obj=user)
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        form.populate_obj(user)
+        db.session.commit()
+        return user.to_dict()
+    
+    return {'errors': form.errors}, 401
