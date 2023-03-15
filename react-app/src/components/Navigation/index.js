62403ef85d25b679c8a3a1c1247684b5beff8746
stevenpicazo <stevenpicazo01@gmail.com>
2023-03-14 23:06:01 -0700

    signed out navbar now displaying

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -65,8 +65,40 @@ function Navigation({ isLoaded }) {
 		return truncatedText.slice(0, occupationIndex) + ' ' + occupationWithClass
 	}
 
-	return (
 
+	if (!sessionUser) {
+		return (
+			<div className="nav-bar-container-new">
+				<h1 onClick={handleHome} className="nav-linkedme-logo">
+					<span className="nav-logo-section-1">Linked</span>
+					<span className="nav-logo-section-2">me</span>
+				</h1>
+				<div className="nav-login-signup-container">
+					<div className="nav-socials-container">
+						<div className="socials" onClick={() => window.open('https://www.linkedin.com/in/steven-picazo-994042225', '_blank')}>
+							<i className="fa-solid fa-link"></i>
+							<span className="nav-socials-title">LinkedIn</span>
+						</div>
+						<div className="socials-end" onClick={() => window.open('https://github.com/stevenpicazo')}>
+							<i className="fa-brands fa-github"></i>
+							<span className="nav-socials-title">Github</span>
+						</div>
+					</div>
+					<div className="nav-border"></div>
+					<button className="nav-signup" onClick={handleSignup}>
+						Sign Up
+					</button>
+					<OpenModalButton
+						className="nav-login"
+						buttonText="Log In"
+						modalComponent={<LoginFormModal />}
+					/>
+				</div>
+			</div>
+		)
+	}
+
+	return (
 		<nav className="nav-bar-container">
 			<div className="nav-logo-search-container">
 				<div onClick={handleHomeClick} className="linkedme-logo">
@@ -112,40 +144,6 @@ function Navigation({ isLoaded }) {
 			)}
 		</nav>
 	)
-
-
-	// if (!sessionUser) {
-	// 	return (
-	// 		<div className="nav-bar-container-new">
-	// 			<h1 onClick={handleHome} className="nav-linkedme-logo">
-	// 				<span className="nav-logo-section-1">Linked</span>
-	// 				<span className="nav-logo-section-2">me</span>
-	// 			</h1>
-	// 			<div className="nav-login-signup-container">
-	// 				<div className="nav-socials-container">
-	// 					<div className="socials" onClick={() => window.open('https://www.linkedin.com/in/steven-picazo-994042225', '_blank')}>
-	// 						<i className="fa-solid fa-link"></i>
-	// 						<span className="nav-socials-title">LinkedIn</span>
-	// 					</div>
-	// 					<div className="socials-end" onClick={() => window.open('https://github.com/stevenpicazo')}>
-	// 						<i className="fa-brands fa-github"></i>
-	// 						<span className="nav-socials-title">Github</span>
-	// 					</div>
-	// 				</div>
-	// 				<div className="nav-border"></div>
-	// 				<button className="nav-signup" onClick={handleSignup}>
-	// 					Sign Up
-	// 				</button>
-	// 				<OpenModalButton
-	// 					className="nav-login"
-	// 					buttonText="Log In"
-	// 					modalComponent={<LoginFormModal />}
-	// 				/>
-	// 			</div>
-	// 		</div>
-	// 	)
-	// }
-
 }
 
 
