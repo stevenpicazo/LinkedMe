0df5a2c20e62bfeabd014d21eefbc5df415ab4a5
stevenpicazo <stevenpicazo01@gmail.com>
2023-04-09 20:13:49 -0700

    fixed close modal bug for creating a post

--- react-app/src/context/Modal.js
+++ react-app/src/context/Modal.js
@@ -1,17 +1,16 @@
-import React, { useRef, useState, useContext, useMemo, useEffect } from 'react';
+import React, { useRef, useState, useContext } from 'react';
 import ReactDOM from 'react-dom';
 import './Modal.css';
 
 const ModalContext = React.createContext();
 
-export function ModalProvider({ children, theme, toggleTheme }) {
+export function ModalProvider({ children }) {
   const modalRef = useRef();
   const [modalContent, setModalContent] = useState(null);
   // callback function that will be called when modal is closing
   const [onModalClose, setOnModalClose] = useState(null);
 
   const closeModal = () => {
-    console.log("closeModal called") // add this line to check if closeModal is being called
     setModalContent(null); // clear the modal contents
     // If callback function is truthy, call the callback function and reset it
     // to null:
@@ -21,17 +20,13 @@ export function ModalProvider({ children, theme, toggleTheme }) {
     }
   };
 
-  // Use the theme and toggleTheme props to force a re-render of the modal content
-  const contextValue = useMemo(() => ({
-    modalRef,
-    modalContent,
-    setModalContent,
-    setOnModalClose,
-    closeModal,
-    theme, // add theme prop to the context value
-    toggleTheme // add toggleTheme prop to the context value
-  }), [modalRef, modalContent, setModalContent, setOnModalClose, closeModal, theme, toggleTheme]);
-
+  const contextValue = {
+    modalRef, // reference to modal div
+    modalContent, // React component to render inside modal
+    setModalContent, // function to set the React component to render inside modal
+    setOnModalClose, // function to set the callback function called when modal is closing
+    closeModal // function to close the modal
+  };
 
   return (
     <>
@@ -45,7 +40,6 @@ export function ModalProvider({ children, theme, toggleTheme }) {
 
 export function Modal() {
   const { modalRef, modalContent, closeModal } = useContext(ModalContext);
-  console.log('Modal closeModal called in Modal.js'); // add this line to check if closeModal is being called
   // If there is no div referenced by the modalRef or modalContent is not a
   // truthy value, render nothing:
   if (!modalRef || !modalRef.current || !modalContent) return null;
@@ -62,13 +56,4 @@ export function Modal() {
   );
 }
 
-export const useModal = () => {
-  const { modalRef, modalContent, setModalContent, setOnModalClose, closeModal, theme, toggleTheme } = useContext(ModalContext);
-
-  // Force a re-render of the modal content when the theme changes
-  useEffect(() => {
-    setModalContent(modalContent);
-  }, [theme]);
-
-  return { modalRef, modalContent, setModalContent, setOnModalClose, closeModal, theme, toggleTheme };
-};
+export const useModal = () => useContext(ModalContext);
